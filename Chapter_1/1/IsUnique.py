1a54bc12502b1991b6087ddc2ffe4098f4ac7f8a
nitrozyna <kurkiewicz.margaret@gmail.com>
2019-12-01 13:15:32 +0100

    Chapter 1 question 1

---
+++ Chapter_1/1/IsUnique.py
@@ -0,0 +1,14 @@
+def isUnique(characters):
+    char_dict = {}
+    for char in characters:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+
+            return False
+    return True
+
+
+# print(isUnique("sdfGd"))
+
+
